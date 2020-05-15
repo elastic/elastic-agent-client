46fc01ebcf66bf63ee07d700550294aedbbc8ae8
Michal Pristas <michal.pristas@gmail.com>
2020-05-15 10:05:05 +0200

    initial

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/elastic/elastic-agent-client/v7
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.4.2
+	github.com/hashicorp/go-multierror v1.1.0
+	github.com/magefile/mage v1.9.0
+	google.golang.org/grpc v1.29.1
+)
---
+++ magefile.go
@@ -0,0 +1,76 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+// +build mage
+
+package main
+
+import (
+	"os"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+)
+
+const (
+	goLintRepo     = "golang.org/x/lint/golint"
+	goLicenserRepo = "github.com/elastic/go-licenser"
+)
+
+// Aliases for commands required by master makefile
+var Aliases = map[string]interface{}{
+	"fmt": Format.All,
+}
+
+// Prepare tasks related to bootstrap the environment or get information about the environment.
+type Prepare mg.Namespace
+
+// Format automatically format the code.
+type Format mg.Namespace
+
+// InstallGoLicenser install go-licenser to check license of the files.
+func (Prepare) InstallGoLicenser() error {
+	return GoGet(goLicenserRepo)
+}
+
+// InstallGoLint for the code.
+func (Prepare) InstallGoLint() error {
+	return GoGet(goLintRepo)
+}
+
+// Update generates client/server code based on proto definition.
+func Update() error {
+	return sh.RunV("protoc", "--go_out=plugins=grpc:.", "elastic-agent-client.proto")
+}
+
+// All format automatically all the codes.
+func (Format) All() {
+	mg.SerialDeps(Format.License)
+}
+
+// License applies the right license header.
+func (Format) License() error {
+	mg.Deps(Prepare.InstallGoLicenser)
+	return combineErr(
+		sh.RunV("go-licenser", "-license", "Elastic"),
+	)
+}
+
+// GoGet fetch a remote dependencies.
+func GoGet(link string) error {
+	_, err := sh.Exec(map[string]string{"GO111MODULE": "off"}, os.Stdout, os.Stderr, "go", "get", link)
+	return err
+}
+
+func combineErr(errors ...error) error {
+	var e error
+	for _, err := range errors {
+		if err == nil {
+			continue
+		}
+		e = multierror.Append(e, err)
+	}
+	return e
+}
