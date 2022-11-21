842aa86a98ca86df2d8c54132dcdadbdd6e845ae
Alex Kristiansen <alex.kristiansen@elastic.co>
2022-11-21 11:34:26 -0800

    cleanup tests

--- pkg/client/unit_test.go
+++ pkg/client/unit_test.go
@@ -8,68 +8,51 @@ import (
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
-func TestUnitUpdateWithSameMap(t *testing.T) {
-	sameStruct := map[string]interface{}{"username": "test"}
-	pbStruct, err := structpb.NewStruct(sameStruct)
-	require.NoError(t, err)
-	existingUnit := Unit{
-		exp:       UnitStateHealthy,
-		logLevel:  UnitLogLevelDebug,
-		configIdx: 1,
-		config:    &proto.UnitExpectedConfig{Source: pbStruct},
-	}
+var defaultTest = Unit{
+	exp:       UnitStateHealthy,
+	logLevel:  UnitLogLevelDebug,
+	configIdx: 1,
+	config:    &proto.UnitExpectedConfig{},
+}
 
-	pbStructNew, err := structpb.NewStruct(sameStruct)
+func TestUnitUpdateWithSameMap(t *testing.T) {
+	defaultTest.configIdx = 1
+	sameMap := map[string]interface{}{"username": "test"}
+	pbStruct, err := structpb.NewStruct(sameMap)
+	require.NoError(t, err)
+	defaultTest.config.Source = pbStruct
+
+	pbStructNew, err := structpb.NewStruct(sameMap)
 	newUnit := &proto.UnitExpectedConfig{
 		Source: pbStructNew,
 	}
 	// This should return false, as the two underlying maps in `source` are the same
-	result := existingUnit.updateState(UnitStateHealthy, UnitLogLevelDebug, newUnit, 2)
+	result := defaultTest.updateState(UnitStateHealthy, UnitLogLevelDebug, newUnit, 2)
 	require.False(t, result)
 }
 
 func TestUnitUpdateWithNewMap(t *testing.T) {
+	defaultTest.configIdx = 1
 	pbStruct, err := structpb.NewStruct(map[string]interface{}{"username": "test"})
 	require.NoError(t, err)
-	existingUnit := Unit{
-		exp:       UnitStateHealthy,
-		logLevel:  UnitLogLevelDebug,
-		configIdx: 1,
-		config:    &proto.UnitExpectedConfig{Source: pbStruct},
-	}
+	defaultTest.config.Source = pbStruct
 
 	pbStructNew, err := structpb.NewStruct(map[string]interface{}{"username": "other"})
+	require.NoError(t, err)
 	newUnit := &proto.UnitExpectedConfig{
 		Source: pbStructNew,
 	}
-
 	// This should return true, as we have an actually new map
-	result := existingUnit.updateState(UnitStateHealthy, UnitLogLevelDebug, newUnit, 2)
+	result := defaultTest.updateState(UnitStateHealthy, UnitLogLevelDebug, newUnit, 2)
 	require.True(t, result)
 }
 
 func TestUnitUpdateLog(t *testing.T) {
-	existingUnit := Unit{
-		exp:       UnitStateHealthy,
-		logLevel:  UnitLogLevelDebug,
-		configIdx: 1,
-		config:    &proto.UnitExpectedConfig{},
-	}
-
-	// This should return true, as we have an actually new map
-	result := existingUnit.updateState(UnitStateHealthy, UnitLogLevelInfo, &proto.UnitExpectedConfig{}, 2)
+	result := defaultTest.updateState(UnitStateHealthy, UnitLogLevelInfo, &proto.UnitExpectedConfig{}, 2)
 	require.True(t, result)
 }
 
 func TestUnitUpdateState(t *testing.T) {
-	existingUnit := Unit{
-		exp:       UnitStateHealthy,
-		logLevel:  UnitLogLevelDebug,
-		configIdx: 1,
-		config:    &proto.UnitExpectedConfig{},
-	}
-
-	// This should return true, as we have an actually new map
-	result := existingUnit.updateState(UnitStateStopped, UnitLogLevelInfo, &proto.UnitExpectedConfig{}, 2)
+	result := defaultTest.updateState(UnitStateStopped, UnitLogLevelInfo, &proto.UnitExpectedConfig{}, 2)
 	require.True(t, result)
 }
