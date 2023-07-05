1060414bde899ddd41543ea00bf3f60b7d84be46
Svetlana <5437289+leo-ri@users.noreply.github.com>
2023-07-05 14:59:15 +0200

    verbosely

--- .buildkite/scripts/update.sh
+++ .buildkite/scripts/update.sh
@@ -6,7 +6,7 @@ source .buildkite/scripts/pre-install-command.sh
 apt-get update
 apt-get install unzip
 curl -sSfL -o protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.19.4/protoc-3.19.4-linux-x86_64.zip
-unzip -o protoc.zip -d /usr/local/bin/protoc
+unzip -ov protoc.zip -d /usr/local/bin/protoc
 which protoc
 export PATH="/usr/local/bin/protoc/bin:$PATH"
 
