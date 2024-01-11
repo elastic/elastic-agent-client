bcb2a6c5bf1d47ed836b7151f17f5831c97bf8d5
Anderson Queiroz <anderson.queiroz@elastic.co>
2024-01-11 11:17:33 +0100

    read new startUpInfo

--- pkg/client/reader.go
+++ pkg/client/reader.go
@@ -64,46 +64,51 @@ func NewFromReader(reader io.Reader, impl StateInterface, actions ...Action) (Cl
 
 // NewV2FromReader creates a new V2 client reading the connection information from the io.Reader.
 func NewV2FromReader(reader io.Reader, ver VersionInfo, opts ...V2ClientOption) (V2, []Service, error) {
-	connInfo := &proto.ConnInfo{}
+	info := &proto.StartupInfo{}
 	data, err := ioutil.ReadAll(reader)
 	if err != nil {
 		return nil, nil, err
 	}
-	err = protobuf.Unmarshal(data, connInfo)
+	err = protobuf.Unmarshal(data, info)
 	if err != nil {
 		return nil, nil, err
 	}
-	if connInfo.Services == nil {
+
+	if info.AgentInfo!=nil{
+		ver.Version=info.AgentInfo.Version
+	}
+
+	if info.ConnInfo.Services == nil {
 		return nil, []Service{ServiceCheckin}, ErrV2Unavailable
 	}
-	cert, err := tls.X509KeyPair(connInfo.PeerCert, connInfo.PeerKey)
+	cert, err := tls.X509KeyPair(info.ConnInfo.PeerCert, info.ConnInfo.PeerKey)
 	if err != nil {
 		return nil, nil, err
 	}
 	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(connInfo.CaCert)
+	caCertPool.AppendCertsFromPEM(info.ConnInfo.CaCert)
 	trans := credentials.NewTLS(&tls.Config{
-		ServerName:   connInfo.ServerName,
+		ServerName:   info.ConnInfo.ServerName,
 		Certificates: []tls.Certificate{cert},
 		RootCAs:      caCertPool,
 	})
-	for _, s := range connInfo.Supports {
+	for _, s := range info.ConnInfo.Supports {
 		if s == proto.ConnectionSupports_CheckinChunking {
 			opts = append(opts, WithChunking(true))
 		}
 	}
-	if connInfo.MaxMessageSize > 0 {
-		opts = append(opts, WithMaxMessageSize(int(connInfo.MaxMessageSize)))
+	if info.ConnInfo.MaxMessageSize > 0 {
+		opts = append(opts, WithMaxMessageSize(int(info.ConnInfo.MaxMessageSize)))
 	}
 	opts = append(opts, WithGRPCDialOptions(grpc.WithTransportCredentials(trans)))
 	client := NewV2(
-		connInfo.Addr,
-		connInfo.Token,
+		info.ConnInfo.Addr,
+		info.ConnInfo.Token,
 		ver,
 		opts...,
 	)
-	services := make([]Service, 0, len(connInfo.Services))
-	for _, srv := range connInfo.Services {
+	services := make([]Service, 0, len(info.ConnInfo.Services))
+	for _, srv := range info.ConnInfo.Services {
 		services = append(services, Service(srv))
 	}
 	return client, services, nil
