cbda909461cdcb31f41a4d34dd17c6ca1d4c961f
Victor Martinez <VictorMartinezRubio@gmail.com>
2025-03-27 12:07:22 +0100

    skip for now

--- pkg/client/chunk/expected_test.go
+++ pkg/client/chunk/expected_test.go
@@ -6,6 +6,7 @@ package chunk
 
 import (
 	"fmt"
+	"runtime"
 	"slices"
 	"strings"
 	"testing"
@@ -254,6 +255,9 @@ func TestExpected(t *testing.T) {
 }
 
 func TestRecvExpected_Timestamp_Restart(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Skipping test on Windows for now. See https://github.com/elastic/elastic-agent-client/issues/120#issuecomment-2757564351")
+	}
 	firstTimestamp := time.Now()
 	first := &proto.CheckinExpected{
 		FeaturesIdx:  2,
