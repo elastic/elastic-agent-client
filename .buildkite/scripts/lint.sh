b2c10e44d2fa19c8733a649b65c9317c9f9fe54d
Victor Martinez <VictorMartinezRubio@gmail.com>
2025-03-27 11:55:34 +0100

    Revert "use only github actions"
    This reverts commit 8784ed35270d91685ba28353106468a18871ff0a.

---
+++ .buildkite/scripts/lint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "--- Prepare enviroment"
+source .buildkite/scripts/pre-install-command.sh
+add_bin_path
+with_mage
+
+echo "--- Run linter"
+mage -debug check
+
+echo "--- Run Go Vet"
+go vet ./...
